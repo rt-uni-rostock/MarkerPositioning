9e8bbe40e16a569b9e7056b00101150fbf3963cb
Martin Guelland <martin.guelland@uni-rostock.de>
2026-03-04 16:15:33 +0100

    added video stream udp port config

--- IO/ImageSource/LiveImageSource/RTPStream/RTPStream.cpp
+++ IO/ImageSource/LiveImageSource/RTPStream/RTPStream.cpp
@@ -30,20 +30,8 @@ void RTPStream::open()
 	LOG_TRACE("Opening Gstreamer RTP stream...");
 
 	std::string pipeline =
-		std::string("v4l2src device=/dev/video") + std::to_string(settings_.streamId) + " do-timestamp=true ! "
-		"image/jpeg,width=1600,height=1200,framerate=15/1 ! "
-		"jpegdec ! "
-		"tee name=t "
-
-		"t. ! queue ! "
-		"videoconvert ! video/x-raw,format=BGR ! "
-		"appsink name=appsink drop=true max-buffers=2 sync=false "
-
-		"t. ! queue ! "
-		"videoconvert ! "
-		"x264enc tune=zerolatency speed-preset=ultrafast bitrate=2000 key-int-max=15 bframes=0 ! "
-		"rtph264pay config-interval=1 pt=96 ! "
-		"udpsink host=192.168.3.35 port=5602 sync=false async=false";
+		std::string("v4l2src device=/dev/video") + std::to_string(settings_.streamId) + std::string(" do-timestamp=true ! image/jpeg,width=1600,height=1200,framerate=15/1 ! jpegdec ! tee name=t t. ! queue ! videoconvert ! video/x-raw,format=BGR ! appsink name=appsink drop=true max-buffers=2 sync=false t. ! queue ! videoconvert ! x264enc tune=zerolatency speed-preset=ultrafast bitrate=2000 key-int-max=15 bframes=0 ! rtph264pay config-interval=1 pt=96 ! ") +
+		std::string("udpsink host=192.168.3.35 port=560") + std::to_string(settings_.streamId) + " sync = false async = false";
 
 	cap.open(pipeline, cv::CAP_GSTREAMER);
 	if (!cap.isOpened())
